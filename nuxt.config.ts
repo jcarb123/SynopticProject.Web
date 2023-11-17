dda16a0b987f58b1d13c89e549ebb853e6aca5fb
James.Carberry <146824494+jcarb123@users.noreply.github.com>
2023-11-17 07:41:49 +0000

    added base project with components

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'ThAmCo Web', 
+      meta: [
+        {
+          name: 'description',
+          content: 'Everything about Nuxt3'
+        }
+      ],
+      link: [
+        {
+          rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+        }
+      ]
+    }
+  }
+})
---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p> {{ data }}</p>
+  </div>
+</template>
+
+<script setup>
+  const { data } = await useFetch('/api/ninja')
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
