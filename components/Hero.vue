4a3816f47acb64703c23d0e5ed9d08a30be41ecc
jcarb123 <146824494+jcarb123@users.noreply.github.com>
2023-12-01 08:53:30 +0000

    try again

--- components/Hero.vue
+++ components/Hero.vue
@@ -91,9 +91,8 @@ const login = () => {
 };
 
 const logout = () => {
-  auth0?.logout({
-    returnTo: 'https://zealous-mud-0b8b86403.4.azurestaticapps.net'
-  });
+  auth0?.logout();
+  navigateTo("https://zealous-mud-0b8b86403.4.azurestaticapps.net");
 };
 
 const toggleProfileModal = () => {
